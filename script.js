c0b189f1b36fc455a41876dc689da74e12b3dd04
YumanGong <gongymsilvia@gmail.com>
2023-11-07 19:42:22 -0600

    update

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ window.addEventListener("load", function () {
 
     let listedPlanets;
     // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-    let listedPlanetsResponse;
+    let listedPlanetsResponse = scriptHelper.myFetch();
     listedPlanetsResponse.then(function (result) {
         listedPlanets = result;
         // console.log(listedPlanets);
